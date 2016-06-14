f79888917a14e912daca83eaf0478209c3d3ff4a
Vishal Bardoloi <vishal.bardoloi@headspring.com>
2016-06-14 14:36:13 -0500

    End of session 1 of Fizzbuzz randori

---
+++ FizzbuzzRandori/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzbuzzRandori
+{
+    public class FizzBuzz
+    {
+        public int GetNumberQty()
+        {
+            return 100;
+        }
+
+        public string[] GetMessagesToShow()
+        {
+            var msgs = new string[100];
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i%3 == 0)
+                {
+                    msgs[i - 1] = "Fizz";
+                }
+                else
+                {
+                    msgs[i - 1] = (i).ToString();
+                }
+            }
+
+            return msgs;
+        }
+    }
+}
---
+++ FizzbuzzTest/FizzBuzzTests.cs
@@ -0,0 +1,47 @@
+﻿namespace FizzbuzzTest
+{
+    using System.Linq;
+    using FizzbuzzRandori;
+    using Shouldly;
+
+    public class FizzBuzzTests
+    {
+        private readonly FizzBuzz _fizzBuzz;
+
+        public FizzBuzzTests()
+        {
+            this._fizzBuzz = new FizzBuzz();
+        }
+
+        // print 1-100
+        // any number divisible by 3 - Fizz
+        // any number divisible by 5 - Buzz
+        // any number divisible by 3 & 5 - FizzBuzz
+
+        public void ShouldHave100Numbers()
+        {
+            _fizzBuzz.GetNumberQty().ShouldBe(100);
+        }
+
+        public void FirstElementReturnedShouldBe1()
+        {
+            _fizzBuzz.GetMessagesToShow().First().ShouldBe("1");
+        }
+
+        public void ShouldHaveSequentialNumbers()
+        {
+            _fizzBuzz.GetMessagesToShow()[0].ShouldBe("1");
+            _fizzBuzz.GetMessagesToShow()[1].ShouldBe("2");
+            _fizzBuzz.GetMessagesToShow()[49].ShouldBe("50");
+            _fizzBuzz.GetMessagesToShow()[99].ShouldBe("100");
+        }
+
+        public void ShouldSayFizzWhenPositionIsDivisibleBy3()
+        {
+            var messages = _fizzBuzz.GetMessagesToShow();
+
+            messages[2].ShouldBe("Fizz");
+            messages[5].ShouldBe("Fizz");
+        }
+    }
+}
